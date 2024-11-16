149bbd376abd922b046e93a5424d3aa98f66a853
Noon_sae <minseock7308@gmail.com>
2024-11-17 04:42:54 +0900

    📝회원가입 페이지 작성

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+
+const SignUpPage = () => {
+    const [formData, setFormData] = useState({
+        userName: '',
+        userId: '',        
+        password: '',
+        checkPassword: '',
+    });
+
+    const { userName, userId, password, checkPassword } = formData;
+
+    const handleChange = (e) => {
+        const { userName, value } = e.target;
+        setFormData({
+            ...formData,
+            [userName]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // 여기서 회원가입 로직을 처리합니다.
+        console.log('회원가입 데이터:', formData);
+    };
+
+    return (
+        <div className="signUp-container">
+            <h2>회원가입 페이지</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="input-group">
+                    <label htmlFor="userName">닉네임</label>
+                    <input
+                        type="text"
+                        id="userName"
+                        name="userName"
+                        // value={userName}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="userId">아이디</label>
+                    <input
+                        type="userId"
+                        id="userId"
+                        name="userId"
+                        // value={userId}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="password">비밀번호</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        // value={password}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="input-group">
+                    <label htmlFor="checkPassword">비밀번호</label>
+                    <input
+                        type="password"
+                        id="checkPassword"
+                        name="checkPassword"
+                        // value={checkPassword}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <button type="submit">회원가입</button>
+            </form>
+        </div>
+    );
+};
+
+export default SignUpPage;
