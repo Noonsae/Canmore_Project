ba99c06d18d7145cefbd039aadf7c7b7db4fffa5
Noon_sae <minseock7308@gmail.com>
2024-11-17 04:38:24 +0900

    📝Provider 생성

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,18 +1,13 @@
-import React from 'react'
+import React from "react";
 import { useState } from "react";
-import { createContext } from 'react'
+import { createContext } from "react";
 
 export const MyContext = createContext();
 
 const Provider = ({ children }) => {
-
   const [userName, setUseName] = useState([]);
 
-  return (
-    <MyContext.Provider value={{  }}>
-    {children}
-  </MyContext.Provider>
-  )
-}
+  return <MyContext.Provider value={{}}>{children}</MyContext.Provider>;
+};
 
-export default Provider
+export default Provider;
