71eccd63b8bc0e2ccce7b040bb313dcf0e132d3a
Noon_sae <minseock7308@gmail.com>
2024-11-17 15:41:17 +0900

    📦 쑤쑤쑤수파노베(이스)

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,9 @@ import React from "react";
 import Provider from "./context/Provider";
 import Router from "./shared/Router";
 // import Reset from "react"
-import dotenv from 'dotenv';
-import { createClient } from '@supabase/supabase-js';
-
-dotenv.config();
 
 const App = () => {
 
-  const supabaseUrl = process.env.SUPABASE_URL;
-  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-  // supabase 클라이언트 생성
-  const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
   return (
     <Provider>
