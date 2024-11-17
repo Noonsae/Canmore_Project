74af3391b76eb6cb68f8794d29627c019ffb455f
Noon_sae <minseock7308@gmail.com>
2024-11-17 15:05:51 +0900

    📝환경변수 코드에 적용 및 .env파일 추적 해제

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import Provider from "./context/Provider";
 import Router from "./shared/Router";
 // import Reset from "react"
-
+import dotenv from 'dotenv';
+import { createClient } from '@supabase/supabase-js';
 
 const App = () => {
+
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+  dotenv.config();
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
   return (
     <Provider>
       <Router />
