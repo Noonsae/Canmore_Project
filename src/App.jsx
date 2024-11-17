f1ea71f7400d16e028bba0867f71fc2f46ed6781
Noon_sae <minseock7308@gmail.com>
2024-11-17 14:55:42 +0900

    🚧 reset이 작동하지 않아 확인 중

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Provider from "./context/Provider";
 import Router from "./shared/Router";
+// import Reset from "react"
+
 
 const App = () => {
   return (
     <Provider>
       <Router />
+      {/* {Reset} */}
     </Provider>
   );
 };
