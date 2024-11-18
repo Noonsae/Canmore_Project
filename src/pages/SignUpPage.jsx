345b9ca6630a318096d9f69e8785d16b60cc36d1
Noon_sae <minseock7308@gmail.com>
2024-11-19 02:11:04 +0900

    📝회원가입페이지 아이디생성까지 구현

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,86 +1,90 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
+import supabase from '../supabase/supabase';
 
 const SignUpPage = () => {
+  // 1. 회원가입 함수 (auth.signUp)
 
-    // const [formData, setFormData] = useState({
-    //     userName: '',
-    //     userId: '',        
-    //     password: '',
-    //     checkPassword: '',
-    // });
+  const [userName, setUserName] = useState([]);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState();
+  const [passwordCheck, setPasswordCheck] = useState();
+  const [user, setUser] = useState(null);
 
-    // const { userName, userId, password, checkPassword } = formData;
+  const onChangeUserName = (e) => setUserName(e.target.value);
+  const onChangeEmail = (e) => setEmail(e.target.value);
+  const onChangePassword = (e) => setPassword(e.target.value);
+  const onChangePasswordCheck = (e) => setPasswordCheck(e.target.value);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+  const signUpNewUser = async (e) => {
+    e.preventDefault();
+
+      const { data, error } = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+          data: {
+            userName: userName
+          }
+        }
+      });
+
+      console.log("sign-up: ", {data, error});
+      setUser(data.user);
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // 회원가입 로직 처리
-        console.log('회원가입 데이터:', formData);
-    };
+  return (
 
-    return (
-        <div className="signUp-container">
-            <h1>아 몰랑 언젠간 이름이 생길거야 ㅇㅅㅇ</h1>
-            <p>빠르고 쉽게 가입할 수 있습니다.</p>
-            <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                    <label htmlFor="userName">이름</label>
-                    <input
-                        type="text"
-                        id="userName"
-                        name="userName"
-                        // value={userName}
-                        placeholder="사용하실 이름을 다시 입력해주세요."
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="userId">아이디</label>
-                    <input
-                        type="text"
-                        id="userId"
-                        name="userId"
-                        // value={userId}
-                        placeholder="아이디를 다시 입력해주세요."
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="password">비밀번호</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        // value={password}
-                        placeholder="비밀번호를 입력해주세요."
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="input-group">
-                    <label htmlFor="checkPassword">비밀번호 확인</label>
-                    <input
-                        type="password"
-                        id="checkPassword"
-                        name="checkPassword"
-                        // value={checkPassword}              
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">가입 요청</button>
-            </form>
-        </div>
-    );
+    <div className="signUp-container">
+      <h1>아 몰랑 언젠간 이름이 생길거야 ㅇㅅㅇ</h1>
+      <p>빠르고 쉽게 가입할 수 있습니다.</p>
+      <form onSubmit={signUpNewUser}>        
+
+          <label htmlFor="userName">이름</label>
+          <input
+            type="text"
+            id="userName"   
+            value={userName}
+            placeholder="사용하실 이름을 다시 입력해주세요."
+            onChange={onChangeUserName}
+            required
+          />
+
+          <label htmlFor="userId">이메일</label>
+          <input
+            type="email"
+            id="email"            
+            value={email}
+            placeholder="아이디를 다시 입력해주세요."
+            onChange={onChangeEmail}
+            required
+          />
+        
+          <label htmlFor="password">비밀번호</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            autocomplete="off"
+            placeholder="비밀번호를 입력해주세요."
+            onChange={onChangePassword}
+            required
+          />
+        
+          <label htmlFor="checkPassword">비밀번호 확인</label>
+          <input
+            type="password"
+            id="checkPassword"            
+            value={passwordCheck}
+            autocomplete="off"
+            placeholder="비밀번호를 다시 입력해주세요."
+            onChange={onChangePasswordCheck}
+            required
+          />
+
+        <button type='onSubmit'>가입 요청</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpPage;
