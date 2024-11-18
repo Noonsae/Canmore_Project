b2f979fc39e983f8626611d3fb13d90fa0ec1a05
Noon_sae <minseock7308@gmail.com>
2024-11-19 01:18:16 +0900

    📝supabase.js 내용 수정

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,5 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;  
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const VITE_SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const VITE_SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;  
+
+if (!VITE_SUPABASE_URL || !VITE_SUPABASE_ANON_KEY) {
+  throw new Error('아니 뭔가 이상한뎅 ㅇㅅㅇ?');
+}
+
+const supabase = createClient(VITE_SUPABASE_URL, VITE_SUPABASE_ANON_KEY);
+
+export default supabase;
