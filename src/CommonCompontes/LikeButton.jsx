45ba9f768cc86b3ad141ae184b3e362c1131fa35
tkddyd0817 <tkddyd1259@gmail.com>
2024-11-18 18:50:52 +0900

    업버튼 라이크버튼 업로드

---
+++ src/CommonCompontes/LikeButton.jsx
@@ -0,0 +1,35 @@
+// import React from 'react'
+import { useState } from 'react';
+
+function LikeButton() {
+  const [likes, setLikes] = useState({}); // 사용자 ID를 키로 하는 객체로 초기화.
+  const userId = 'user1'; // 예시로 사용자 ID를 지정합니다. 실제로는 로그인한 사용자의 ID를 사용.
+
+  const addLike = () => {
+    setLikes((prevLikes) => ({
+      ...prevLikes,
+      [userId]: (prevLikes[userId] || 0) + 1 // 이전 좋아요 수에 1을 +.
+    }));
+  };
+
+  const removeLike = () => {
+    setLikes((prevLikes) => {
+      const currentLikes = prevLikes[userId] || 0;
+      return {
+        ...prevLikes,
+        [userId]: currentLikes > 0 ? currentLikes - 1 : 0 // 현재 좋아요 수가 0보다 클 때만 감소합니다.
+      };
+    });
+  };
+
+  const totalLikes = Object.values(likes).reduce((acc, count) => acc + count, 0); // 총 좋아요 수 계산
+
+  return (
+    <div>
+      <h3>LikeButton</h3>
+      <button onClick={likes[userId] ? removeLike : addLike}>(❤️{totalLikes})</button>
+    </div>
+  );
+}
+
+export default LikeButton;
