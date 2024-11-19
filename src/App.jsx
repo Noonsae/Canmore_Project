55a3700557aa19b0277d2628fa115397fcc5edf0
bj9322 <bj932218@gmail.com>
2024-11-19 21:46:37 +0900

    📝 FeedProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from "react";
 import Provider from "./context/Provider";
 import Router from "./shared/Router";
 import GlobalStyle from "./styles/GlobalStyle";
+import { FeedProvider } from "./context/FeedContext";
 
 const App = () => {
-
   return (
+    <FeedProvider>      
     <Provider>
-      <Router/>
-      <GlobalStyle/>
+        <GlobalStyle />
+        <Router />
     </Provider>
+    </FeedProvider>
   );
 };
 
