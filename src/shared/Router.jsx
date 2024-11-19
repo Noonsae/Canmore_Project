2e85941ce674114d62df93a9e6dc815dc2124115
Noon_sae <minseock7308@gmail.com>
2024-11-20 08:26:05 +0900

    📝비밀번호재설정 페이지 생성

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import SignUpPage from "../pages/SignUpPage";
 import Home from "../pages/Home";
+import ResetPasswordPage from "../pages/ResetPasswordPage";
 
 
 const Router = () => {
@@ -14,6 +15,8 @@ const Router = () => {
         <Route path="/" element={<LoginPage/>}/>
         <Route path="/sign-up" element={<SignUpPage/>}/>
         <Route path="/Home" element={<Home/>}/>
+        <Route path="/reset" element={<ResetPasswordPage/>}/>
+
       </Routes>
     </BrowserRouter>
   );
