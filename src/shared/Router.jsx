bffb55fa7919b7e302f056d68bf090afe9dedaf6
bj9322 <bj932218@gmail.com>
2024-11-20 00:45:38 +0900

    📝 라우터 경로 수정 앞으로 여러가지 추가해야함

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUpPage from "../pages/SignUpPage";
-import HomePage from "../pages/HomePage";
-import Layout from "../components/Layout";
 import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
 import FeedPage from "../pages/FeedPage";
+import Home from "../pages/Home";
 import TimeLine from "../pages/TimeLine";
+import Layout from "../components/Layout";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/LoginPage" element={<LoginPage />} />
       <Route path="/sign-up" element={<SignUpPage />} />
       <Route path="/FeedPage" element={<FeedPage />} />
-      <Route path="/HomePage" element={<Layout><HomePage /></Layout>}/>
+      <Route path="/" element={<Layout><Home /></Layout>}/>
       <Route path="/timeline" element={<TimeLine/>}/>
+      <Route path="/write" element={<FeedPage />} />
     </Routes>
   </BrowserRouter>
 );
