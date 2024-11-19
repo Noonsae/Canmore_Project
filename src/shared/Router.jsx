9b5c6d99a2d8932d7fecbe9a489651d4dd9a2976
bj9322 <bj932218@gmail.com>
2024-11-19 21:50:53 +0900

    📝 라우터 설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-import LoginPage from "../pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "../pages/SignUpPage";
 import HomePage from "../pages/HomePage";
 import Layout from "../components/Layout";
+import LoginPage from "../pages/LoginPage";
+import FeedPage from "../pages/FeedPage";
+import TimeLine from "../pages/TimeLine";
 
-
-
-const Router = () => {
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route path="/sign-up" element={<SignUpPage/>}/>
-        <Route path="/Home" element={<Layout><HomePage/></Layout>}/>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const Router = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/sign-up" element={<SignUpPage />} />
+      <Route path="/FeedPage" element={<FeedPage />} />
+      <Route path="/HomePage" element={<Layout><HomePage /></Layout>}/>
+      <Route path="/timeline" element={<TimeLine/>}/>
+    </Routes>
+  </BrowserRouter>
+);
 
 export default Router;
