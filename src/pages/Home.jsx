002155f95cb3e232574661cace9f216543e288f4
Noon_sae <minseock7308@gmail.com>
2024-11-20 11:47:21 +0900

    📝네비게이트 값 수정 및 첫 화면으로 돌아가는 버튼 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import supabase from '../supabase/supabase';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const signOutUser = async () => {
+  const handleLogOut = async () => {
     const { data, error } = await supabase.auth.signOut();
     console.log('signout: ', { data, error });
+    useNavigate('/');
   };
 
   return (
     <div>
       <p>Home</p>
-      <button onClick={signOutUser}>로그아웃</button>
+      <button onClick={handleLogOut}>로그아웃</button>
     </div>
   );
 };
