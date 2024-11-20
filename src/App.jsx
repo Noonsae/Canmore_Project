b710a2500ba3bf307c7d431ba884b85b8d2e2205
bj9322 <bj932218@gmail.com>
2024-11-20 23:10:00 +0900

    📝 라우팅 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
 import Router from './shared/Router';
 import GlobalStyle from './styles/GlobalStyle';
 import { FeedProvider } from './context/FeedContext';
-import { useEffect, useState } from 'react';
-import supabase from './supabase/Supabase';
+import {UserProvider} from './context/userContext'
 
 const App = () => {
-  const [userId, setUserId] = useState(null);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      const { data: authData, error } = await supabase.auth.getUser(); // 로그인된 사용자 정보 가져오기
-
-      if (error) {
-        console.error('Error fetching user:', error);
-        alert('로그인된 사용자를 확인할 수 없습니다.');
-        return;
-      }
-      setUserId(authData.user.id || null);
-    };
-
-    fetchUser();
-  }, []);
 
   return (
+    <UserProvider>
     <FeedProvider>
       <GlobalStyle />
-      <Router userId={userId} /> {/* Router 컴포넌트가 이미 모든 경로를 처리 */}
+      <Router /> {/* Router 컴포넌트가 이미 모든 경로를 처리 */}
     </FeedProvider>
+    </UserProvider>
   );
 };
 
