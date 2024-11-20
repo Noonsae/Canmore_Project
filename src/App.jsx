a3eea1ad50f9c7db8725b72348451c639cfc6ffc
Noon_sae <minseock7308@gmail.com>
2024-11-20 12:30:29 +0900

    🔥Provider 파일 삭제했습니다. FeedProvider로 전역관리하겠습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Provider from "./context/Provider";
 import Router from "./shared/Router";
 import GlobalStyle from "./styles/GlobalStyle";
 import { FeedProvider } from "./context/FeedContext";
@@ -6,10 +5,8 @@ import { FeedProvider } from "./context/FeedContext";
 const App = () => {
   return (
     <FeedProvider>
-      <Provider>
         <GlobalStyle />
         <Router /> {/* Router 컴포넌트가 이미 모든 경로를 처리 */}
-      </Provider>
     </FeedProvider>
   );
 };
