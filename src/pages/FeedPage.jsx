9eb124f417c2e3b12b92b1f80e6fbbf5b68a2135
Raina-Moon <mds6425@gmail.com>
2024-11-20 20:45:03 +0900

    🚧 props에 맞춰 수정했지만 기능구현에 대한 수정이 더 필요함

--- src/pages/FeedPage.jsx
+++ src/pages/FeedPage.jsx
@@ -3,13 +3,13 @@ import { useFeed } from '../context/FeedContext';
 import PostForm from '../components/PostForm';
 
 
-const FeedPage = () => {
+const FeedPage = ({userId}) => {
   const { addPost } = useFeed();
 
   return (
     <div>
       <h1>글쓰기</h1>
-      <PostForm onAddPost={addPost} />
+      <PostForm onAddPost={addPost} userId={userId}/>
     </div>
   );
 };
