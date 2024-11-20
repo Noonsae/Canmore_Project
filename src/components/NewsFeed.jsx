9beee38585dc823eda872a9da1726cde382a0974
bj9322 <bj932218@gmail.com>
2024-11-21 00:40:35 +0900

    📝 뉴스피드 스타일 수정

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -10,7 +10,7 @@ const NewsFeed = () => {
         feeds.map((feed, index) => (
           <div key={index} style={{ marginBottom: "16px" }}>
             <p>{feed.content}</p>
-            {feed.image_url && <img src={feed.image_url} alt="첨부 이미지" style={{ maxWidth: "100%" }} />}
+            {feed.image_url && <img src={feed.image_url} alt="첨부 이미지" style={{ maxWidth: "50%" }} />}
           </div>
         ))
       ) : (
